8c4b45c350cd63f9cc390c86939b184ce9e6c0c2
gthayer <gary@hallme.com>
2015-12-24 11:06:53 -0500

    added prompt for localhost values, gave prompts validation

--- gulpfile.js
+++ gulpfile.js
@@ -17,27 +17,67 @@ var cwd = process.env.INIT_CWD;
 // Project Working Directory.
 var pwd = __dirname;
 
-// System Specific Variables.
-// Builds a config file if none exist.
-// TODO: As more variables are needed, build in prompts for the setup. wptemplate may be going away,
-// as template location may not be important now that WP is grabbed through git.
-try {
-	var config = require( './config.json' );
-} catch (ex) {
+// System Variables.
+var config;
+var response;
 
-	var config = {
-		localhost: pwd,
-		wptemplate: pwd + "/wordpress-template"
-	};
+gulp.task( 'build-config', function( callback ) {
+	// Builds a config file if none exist.
+	try {
+		config = require( './config.json' );
+		callback();
+	} catch (ex) {
 
-	var config_log = JSON.stringify( config, null, '\t' );
+		console.log( "Let's get some info about your environment:" );
 
-	fs.writeFile( 'config.json', config_log, function ( err ) {
-		if ( err ) {
-			throw err;
-		}
-	});
-}
+		gulp.src( 'gulpfile.js' )
+			.pipe( prompt.prompt([{
+				type: 'input',
+				name: 'db_user',
+				message: 'Database Username?',
+				validate: function( db_user ) {
+					if ( db_user.length > 0 ) {
+						return true;
+					}
+				}
+			},
+			{
+				type: 'input',
+				name: 'db_pass',
+				message: 'Database Password?',
+				validate: function( db_pass ){
+					if ( db_pass.length > 0 ) {
+						return true;
+					}
+				}
+			},
+			{
+				type: 'input',
+				name: 'db_host',
+				message: 'Database Host?',
+				validate: function( db_host ){
+					if ( db_host.length > 0 ) {
+						return true;
+					}
+				}
+			}], function ( res ) {
+				config = {
+					localhost: pwd,
+					wptemplate: pwd + "/wordpress-template",
+					db_user: res.db_user,
+					db_pass: res.db_pass,
+					db_host: res.db_host
+				};
+				var config_log = JSON.stringify( config, null, '\t' );
+				fs.writeFile( 'config.json', config_log, function ( err ) {
+					if ( err ) {
+						throw err;
+					}
+				});
+				callback();
+			}));
+	}
+});
 
 // Asset Directory.
 var assets_dir = cwd;
@@ -88,32 +128,29 @@ gulp.task( 'live-reload', function () {
 // Default.
 gulp.task( 'default', ['compass', 'watch'] );
 
-var response;
-
-gulp.task( 'install-prompt', function( callback ) {
+gulp.task( 'install-prompt', ['build-config'], function( callback ) {
 	gulp.src( 'gulpfile.js' )
 		.pipe( prompt.prompt( {
 			type: 'input',
 			name: 'installname',
-			message: 'Install Name?'
+			message: 'Install Name?',
+			validate: function( installname ){
+				if ( installname.length > 0 ) {
+					return true;
+				}
+			}
 		}, function ( res ) {
 			response = res;
-			if ( res.installname ) {
-				// Check if a local version of the WordPress repo exisits. If not, build one.
-				if ( ! fs.existsSync( './wordpress-template' ) ) {
-					git.clone( 'https://github.com/WordPress/WordPress.git', {args: '--depth 1 ./wordpress-template'}, function (err) {
-						if ( err ) {
-							throw err;
-						}
-						callback();
-					});
-				} else {
+			// Check if a local version of the WordPress repo exisits. If not, build one.
+			if ( ! fs.existsSync( './wordpress-template' ) ) {
+				git.clone( 'https://github.com/WordPress/WordPress.git', {args: '--depth 1 ./wordpress-template'}, function (err) {
+					if ( err ) {
+						throw err;
+					}
 					callback();
-				}
+				});
 			} else {
-				// Error message if input is left blank.
-				console.log( 'ERROR: Please enter an install name' );
-				return;
+				callback();
 			}
 		}));
 });
@@ -129,14 +166,16 @@ gulp.task( 'copytocwd', ['install-prompt'], function(callback) {
 	.on( 'end', callback );
 });
 
-gulp.task( 'buildlocal', ['install-prompt', 'copytocwd'], function() {
+gulp.task( 'buildlocal', ['build-config', 'install-prompt', 'copytocwd'], function() {
 	if ( ! fs.existsSync( cwd + '/wp-config.php' ) ) {
 		fs.readFile(cwd + '/wp-config-sample.php', 'utf8', function (err,data) {
 			if ( err ) {
 				throw err;
 			}
-			// TODO: Build prompt into config.json creation which grabs values for automatic wp-config creation.
-			var result = data.replace( "define('DB_NAME', 'database_name_here');", "define('DB_NAME', '" + response.installname + "');" );
+			var result = data.replace( "define('DB_NAME', 'database_name_here');", "define('DB_NAME', '" + response.installname + "');" )
+						.replace( "define('DB_USER', 'username_here');", "define('DB_USER', '" + config.db_user + "');" )
+						.replace( "define('DB_PASSWORD', 'password_here');", "define('DB_PASSWORD', '" + config.db_pass + "');" )
+						.replace( "define('DB_HOST', 'localhost');", "define('DB_HOST', '" + config.db_host + "');" );
 			fs.writeFile(cwd + '/wp-config.php', result, 'utf8', function (err) {
 				if ( err ) {
 					throw err;
